efc05b21921f02d9c8cb60497b15af202640134b
Supakit Inkaew <tonsupakitinkeaw@gmail.com>
2018-08-16 18:38:24 +0700

    first_commit

---
+++ Assets/Script/loadScene.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loadScene : MonoBehaviour {
+		public void numScence(int sceneNumber) {
+      Application.LoadLevel(sceneNumber);
+    }
+}
