48be80110ce54170445ccfcf3b271076b2de5465
Supakit Inkaew <tonsupakitinkeaw@gmail.com>
2018-10-22 15:02:25 +0700

    hkb_newversion_fixlayout

---
+++ Assets/Script/ExternalLink.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExternalLink : MonoBehaviour {
+
+	public void OpenFacebook () {
+		Application.OpenURL ("https://www.facebook.com/huenkhambai/");
+	}
+}
